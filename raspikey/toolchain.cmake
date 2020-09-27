a788399466de77578eff0b5973e380e38395fc69
wp吴鹏(p_wu) <p_wu@ctrip.com>
2020-09-27 16:39:54 +0800

    update config for local dev

--- raspikey/toolchain.cmake
+++ raspikey/toolchain.cmake
@@ -3,8 +3,8 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv6)
 set(triple arm-linux-gnueabihf)
 
-set(TOOLCHAIN_SYSROOT /SysGCC/raspberry/arm-linux-gnueabihf/sysroot)
-set(TOOLCHAIN_BIN /SysGCC/raspberry/bin)
+set(TOOLCHAIN_SYSROOT d:/tools/raspberry-gcc/arm-linux-gnueabihf/sysroot)
+set(TOOLCHAIN_BIN d:/tools/raspberry-gcc/bin)
 
 set(CMAKE_SYSROOT ${TOOLCHAIN_SYSROOT})
 set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_SYSROOT})
