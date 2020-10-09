ef4579fa2af8ed239ca60711e220aa2f5f296c4f
samartzidis <samartzidis@gmail.com>
2020-10-09 18:37:26 +0100

    Updated build scripts

--- raspikey/configure.sh
+++ raspikey/configure.sh
@@ -5,3 +5,4 @@ cmake -G'Unix Makefiles' -Bbuild/Debug --no-warn-unused-cli -DCMAKE_EXPORT_COMPI
 
 echo "Configuring Release..."
 cmake -G'Unix Makefiles' -Bbuild/Release --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_TOOLCHAIN_FILE:FILEPATH=toolchain-rpi0.cmake -H.
+
